9cefdf91a3e6fe3e71d0e16a953cd34af64ca83b
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2021-10-03 18:53:58 -0700

    Add comments and move sensor keys. (#21)
    * Add comments and move sensor keys.
    
    * Add a sensor hostname path.
    
    * Typo

--- client.go
+++ client.go
@@ -37,12 +37,12 @@ type Identity struct {
 }
 
 type ClientOptions struct {
-	Identity      Identity                    `json:"identity" yaml:"identity"`
-	Hostname      string                      `json:"hostname,omitempty" yaml:"hostname,omitempty"`
-	Platform      string                      `json:"platform,omitempty" yaml:"platform,omitempty"`
-	Architecture  string                      `json:"architecture,omitempty" yaml:"architecture,omitempty"`
-	Mapping       *protocol.MappingDescriptor `json:"mapping,omitempty" yaml:"mapping,omitempty"`
-	BufferOptions AckBufferOptions            `json:"buffer_options,omitempty" yaml:"buffer_options,omitempty"`
+	Identity      Identity                   `json:"identity" yaml:"identity"`
+	Hostname      string                     `json:"hostname,omitempty" yaml:"hostname,omitempty"`
+	Platform      string                     `json:"platform,omitempty" yaml:"platform,omitempty"`
+	Architecture  string                     `json:"architecture,omitempty" yaml:"architecture,omitempty"`
+	Mapping       protocol.MappingDescriptor `json:"mapping,omitempty" yaml:"mapping,omitempty"`
+	BufferOptions AckBufferOptions           `json:"buffer_options,omitempty" yaml:"buffer_options,omitempty"`
 
 	SensorKeyPath string `json:"sensor_key_path" yaml:"sensor_key_path"`
 	SensorSeedKey string `json:"sensor_seed_key" yaml:"sensor_seed_key"`
@@ -83,7 +83,7 @@ func NewClient(o ClientOptions) (*Client, error) {
 		o.Hostname, _ = os.Hostname()
 	}
 
-	if o.Mapping != nil && o.Mapping.ParsingRE != "" {
+	if o.Mapping.ParsingRE != "" {
 		if _, err := regexp.Compile(o.Mapping.ParsingRE); err != nil {
 			return nil, fmt.Errorf("invalid mapping.parsing_re: %v", err)
 		}
@@ -139,7 +139,6 @@ func (c *Client) connect() error {
 		Platform:        c.options.Platform,
 		Architecture:    c.options.Architecture,
 		Mapping:         c.options.Mapping,
-		SensorKeyPath:   c.options.SensorKeyPath,
 		SensorSeedKey:   c.options.SensorSeedKey,
 	}); err != nil {
 		c.log(fmt.Sprintf("usp-client WriteJSON(): %v", err))
--- protocol/mapping.go
+++ protocol/mapping.go
@@ -1,12 +1,28 @@
 package protocol
 
 type MappingDescriptor struct {
-	ParsingRE    string         `json:"parsing_re,omitempty" yaml:"parsing_re,omitempty"`
-	IsRenameOnly bool           `json:"rename_only" yaml:"rename_only"`
-	Mappings     []FieldMapping `json:"mappings" yaml:"mappings"`
+	// Use the named capture fields from the regular
+	// expression below to parse text lines into JSON.
+	ParsingRE string `json:"parsing_re,omitempty" yaml:"parsing_re,omitempty"`
+
+	// Path to the component of the JSON events that
+	// indicates unique values to become Sensor IDs.
+	SensorKeyPath string `json:"SENSOR_KEY_PATH,omitempty"`
+
+	// Path to the component of the JSON events that
+	// indicates the hostname of the sensor per the SensorKeyPath.
+	SensorHostnamePath string `json:"SENSOR_HOSTNAME_PATH,omitempty"`
+
+	// Rename the fields in the Mappings but leave
+	// all other fields in place. The default behavior
+	// is to replace the final event with only the
+	// mapped values.
+	IsRenameOnly bool           `json:"rename_only,omitempty" yaml:"rename_only,omitempty"`
+	Mappings     []FieldMapping `json:"mappings,omitempty" yaml:"mappings,omitempty"`
 }
 
 type FieldMapping struct {
+	// Map the source field to the destination field.
 	SourceField      string `json:"src_field" yaml:"src_field"`
 	DestinationField string `json:"dst_field" yaml:"dst_field"`
 }
